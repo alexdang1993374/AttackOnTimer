99e13eee3610e53360c55be72e3336cb0b9a1f2d
unknown <alexdang1993374@gmail.com>
2021-01-15 22:26:16 +0700

    finished day counter

---
+++ app.js
@@ -0,0 +1,32 @@
+function goToAnime() {
+  window.open("https://www12.9anime.to/watch/attack-on-titan-final-season.k524/ep-1")
+}
+
+function updateClock() {
+  var now = new Date(), // current date
+    Day = now.getDay();
+  if (Day == 1) {
+    document.getElementById("time").textContent = " New episode is out today!";
+  }
+  if (Day == 2) {
+    document.getElementById("time").textContent = " New episode will air in 6 days!";
+  }
+  if (Day == 3) {
+    document.getElementById("time").textContent = " New episode will air in 5 days!";
+  }
+  if (Day == 4) {
+    document.getElementById("time").textContent = " New episode will air in 4 days!";
+  }
+  if (Day == 5) {
+    document.getElementById("time").textContent = " New episode will air in 3 days!";
+  }
+  if (Day == 6) {
+    document.getElementById("time").textContent = " New episode will air in 2 days!";
+  }
+  if (Day == 7) {
+    document.getElementById("time").textContent = " New episode will air tomorrow!";
+  }
+
+  setTimeout(updateClock, 1000);
+}
+updateClock();
